b16cad10d89636c254cdb4b722d0adf627f1bff1
NEWTOWNBOY <38236367+kojihun@users.noreply.github.com>
2024-04-23 14:54:32 +0900

    initial commit

---
+++ src/main/java/com/swyg/oneului/OneuluiApplication.java
@@ -0,0 +1,13 @@
+package com.swyg.oneului;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneuluiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OneuluiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/swyg/oneului/OneuluiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swyg.oneului;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OneuluiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
