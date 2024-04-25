7e6df6f60471f53204ecd3dea5c44c142a517cbb
NEWTOWNBOY <38236367+kojihun@users.noreply.github.com>
2024-04-25 12:08:25 +0900

    Feat: "swagger 연동" (#2)
    Feat: "swagger 연동"
    
    API 문서화를 위한 swagger 연동
    
    Resolves: #1

---
+++ src/main/java/com/swyg/oneului/controller/SwaggerTestController.java
@@ -0,0 +1,22 @@
+package com.swyg.oneului.controller;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Tag(name = "Swagger Test", description = "Swagger Test API")
+@RequestMapping("/api/swagger")
+@RequiredArgsConstructor
+@RestController
+public class SwaggerTestController {
+    @Operation(summary = "스웨거 테스트 API", description = "스웨거 테스트 API")
+    @GetMapping("/test")
+    public ResponseEntity<String> test() {
+        return ResponseEntity.ok().body("SUCCESS");
+    }
+}
