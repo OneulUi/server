679787e9a76da23e8938175ea5362a4c565be596
taebong98 <thk98k@gmail.com>
2024-05-02 22:32:22 +0900

    Setting: CORS 정책 수정

--- src/main/java/com/swyg/oneului/controller/HelloController.java
+++ src/main/java/com/swyg/oneului/controller/HelloController.java
@@ -1,13 +1,16 @@
 package com.swyg.oneului.controller;
 
 import com.swyg.oneului.controller.doc.HelloControllerDoc;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class HelloController implements HelloControllerDoc {
     @GetMapping("/hello")
     public String hello() {
+        log.info("hello 호출");
         return "hello";
     }
 }
