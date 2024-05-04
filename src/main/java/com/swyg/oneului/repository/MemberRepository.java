9e4b17f5f83318222369838194fa9fc75c0f30c9
NEWTOWNBOY <38236367+kojihun@users.noreply.github.com>
2024-05-04 22:11:39 +0900

    Feat: "구글 로그인 기능 수정"
    Feat: "구글 로그인 기능 수정"
    
    - 구글 로그인시 프론트로 리다이렉트가 가능하도록 기능 수정

--- src/main/java/com/swyg/oneului/repository/MemberRepository.java
+++ src/main/java/com/swyg/oneului/repository/MemberRepository.java
@@ -2,6 +2,7 @@ package com.swyg.oneului.repository;
 
 import com.swyg.oneului.model.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -10,9 +11,9 @@ import java.util.List;
 
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
-
     List<Member> findMemberByLoginId(String loginId);
 
+    @Modifying
     @Query("update Member m set m.refreshToken = :refreshToken where m.loginId = :loginId")
     void updateRefreshToken(@Param("refreshToken") String refreshToken, @Param("loginId") String loginId);
 }
