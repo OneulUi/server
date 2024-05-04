744b9836e9343c4d87c1674fc055ec48c17af792
taebong98 <thk98k@gmail.com>
2024-05-04 12:21:42 +0900

    Setting: CORS 정책 수정

--- src/main/java/com/swyg/oneului/config/SecurityConfig.java
+++ src/main/java/com/swyg/oneului/config/SecurityConfig.java
@@ -87,7 +87,8 @@ public class SecurityConfig {
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
-        configuration.setAllowedMethods(Arrays.asList("GET","POST"));
+        configuration.setAllowedMethods(Arrays.asList("GET","POST", "PUT", "DELETE", "OPTIONS"));
+
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
